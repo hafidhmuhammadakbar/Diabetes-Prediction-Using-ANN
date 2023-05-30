8f71a48562ea0608fd8e0be0f0b30324eac1b41b
hafidhmuhammadakbar <hafidhmuhammadakbar1@student.uns.ac.id>
2023-05-30 22:53:20 +0700

    add scaling data

--- data_splitting.py
+++ data_splitting.py
@@ -1,20 +1,27 @@
 from data_cleaning import data
 from sklearn.model_selection import train_test_split
+from sklearn import preprocessing
 
 def main():
     # print the shape of train, test, and validation sets
-    print('train shape: ', train.shape)
-    print(train)
-    print('test shape: ', test.shape)
-    print(test)
-    print('validation shape: ', validation.shape)
-    print(validation)
+    print('train shape: ', X_train.shape)
+    print(X_train)
+    print('test shape: ', X_test.shape)
+    print(X_test)
 
-# split the data into train and test sets with 70% and 30% respectively and arbitrary random_state
-train, test = train_test_split(data, test_size=0.3, random_state=42)
+# split the data into dependent and independent variables
+# x = independent
+# y = dependent
+X = data.iloc[:,:-1].values
+y = data.iloc[:,-1].values
 
-# split the test data into test and validation sets with 50% and 50% respectively and arbitrary random_state
-test, validation = train_test_split(test, test_size=0.5, random_state=42)
+# split the data into train and test sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, train_size=0.8, random_state=42)
+
+# scaling data
+stand = preprocessing.StandardScaler()
+X_train = stand.fit_transform(X_train)
+X_test = stand.transform(X_test)
 
 # print main
 if __name__ == "__main__":
